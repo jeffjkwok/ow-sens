89d1674bdf68a0dca55967f93cca4bb0f719b7e4
Jeff Kwok <jkwok@athleteintelligence.com>
2017-06-29 16:07:07 -0700

    A little CSS and Basic Backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { SenseComponent } from './sense/sense.component';
@@ -10,7 +11,8 @@ import { SenseComponent } from './sense/sense.component';
     SenseComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/sense/sense.component.ts
+++ src/app/sense/sense.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sense',
@@ -6,10 +7,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sense.component.css']
 })
 export class SenseComponent implements OnInit {
+  start: number;
+  two: number;
+  three: number;
+  four: number;
+  five: number;
+  six: number;
+  seven: number;
+  final: number;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  rowOneLow(){
+    this.two = Math.round((this.start + this.start*0.5)/2*100)/100
+  }
+  rowOneHigh(){
+    this.two = Math.round((this.start + this.start*1.5)/2*100)/100
+  }
+  rowTwoLow(){
+    this.three = Math.round((this.two + this.two*0.5)/2*100)/100
+  }
+  rowTwoHigh(){
+    this.three = Math.round((this.two + this.two*1.5)/2*100)/100
+  }
+  rowThreeLow(){
+    this.four = Math.round((this.three + this.three*0.6)/2*100)/100
+  }
+  rowThreeHigh(){
+    this.four = Math.round((this.three + this.three*1.4)/2*100)/100
+  }
+  rowFourLow(){
+    this.five = Math.round((this.four + this.four*0.7)/2*100)/100
+  }
+  rowFourHigh(){
+    this.five = Math.round((this.four + this.four*1.3)/2*100)/100
+  }
+  rowFiveLow(){
+    this.six = Math.round((this.five + this.five*0.8)/2*100)/100
+  }
+  rowFiveHigh(){
+    this.six = Math.round((this.five + this.five*1.2)/2*100)/100
+  }
+  rowSixLow(){
+    this.seven = Math.round((this.six + this.six*0.9)/2*100)/100
+  }
+  rowSixHigh(){
+    this.seven = Math.round((this.six + this.six*1.1)/2*100)/100
+  }
+  rowSevenLow(){
+    this.final = Math.round((this.seven + this.seven*0.95)/2*100)/100
+  }
+  rowSevenHigh(){
+    this.final = Math.round((this.seven + this.seven*1.05)/2*100)/100
   }
 
 }
