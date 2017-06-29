28f669c3db436792d9dfd752d6a71b818cd805f8
Jeff Kwok <jkwok@athleteintelligence.com>
2017-06-29 13:27:17 -0700

    framework of component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SenseComponent } from './sense/sense.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SenseComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/sense/sense.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sense',
+  templateUrl: './sense.component.html',
+  styleUrls: ['./sense.component.css']
+})
+export class SenseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
